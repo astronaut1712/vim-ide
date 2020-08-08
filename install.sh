b542e19266aaf68f318c4db5f1757fbfc22e6d42
Quang <quang@gicungco>
2020-08-08 12:46:34 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y vim curl python3 python3-dev cmake go xclip
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Linux OS"
+    DISTRO_ID=$(lsb_release -si)
+    if [[ "$DISTRO_ID" == "Ubuntu" ]]; then
+        echo "Install with apt"
+	sudo apt update
+	sudo apt install -y vim curl python3 python3-dev cmake xclip
+    elif [[ "$DISTRO_ID" == "Fedora"* ]]; then
+        echo "Install with yum"
+	sudo yum update
+	sudo yum install -y vim curl python3 python3-dev cmake xclip
+    else
+        echo "error can't install package"
+        exit 1;
+    fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install vim curl cmake python3 python3-dev
+else
+    echo "Unsupport platform: $OSTYPE"
+    exit 1
+fi
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 ~/.nvm/nvm.sh
 nvm install stable
-npm install -g yarn
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
 vim +PluginInstall +qall
